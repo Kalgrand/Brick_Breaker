dd7494e790d1077653ea7db7d263216b7a015746
michal <micdeg96@gmail.com>
2021-05-28 18:36:13 +0200

    game loop

--- Brick_Breaker2.cpp
+++ Brick_Breaker2.cpp
@@ -10,10 +10,39 @@ int ball_vel_x = 1;
 int ball_vel_y = 1;
 int background_w = 800;
 int background_h = 600;
+int brick_w = 80;
+int brick_h = 35;
 int background_min_w = 0;
 int background_min_h = 0;
 int floor_x = background_w / 2;
 int floor_y = background_h - 30;
+SDL_Surface* brick;
+SDL_Texture* brick_texture;
+SDL_Rect brick_rect[3][7];
+
+void InitializerBrick() {
+	brick_rect[0][0] = { 50, 50, brick_w, brick_h };
+	brick_rect[0][1] = { 150, 50, brick_w, brick_h };
+	brick_rect[0][2] = { 250, 50, brick_w, brick_h };
+	brick_rect[0][3] = { 350, 50, brick_w, brick_h };
+	brick_rect[0][4] = { 450, 50, brick_w, brick_h };
+	brick_rect[0][5] = { 550, 50, brick_w, brick_h };
+	brick_rect[0][6] = { 650, 50, brick_w, brick_h };
+	brick_rect[1][0] = { 50, 100, brick_w, brick_h };
+	brick_rect[1][1] = { 150, 100, brick_w, brick_h };
+	brick_rect[1][2] = { 250, 100, brick_w, brick_h };
+	brick_rect[1][3] = { 350, 100, brick_w, brick_h };
+	brick_rect[1][4] = { 450, 100, brick_w, brick_h };
+	brick_rect[1][5] = { 550, 100, brick_w, brick_h };
+	brick_rect[1][6] = { 650, 100, brick_w, brick_h };
+	brick_rect[2][0] = { 50, 150, brick_w, brick_h };
+	brick_rect[2][1] = { 150, 150, brick_w, brick_h };
+	brick_rect[2][2] = { 250, 150, brick_w, brick_h };
+	brick_rect[2][3] = { 350, 150, brick_w, brick_h };
+	brick_rect[2][4] = { 450, 150, brick_w, brick_h };
+	brick_rect[2][5] = { 550, 150, brick_w, brick_h };
+	brick_rect[2][6] = { 650, 150, brick_w, brick_h };
+}
 
 void EventHandler() {
 	SDL_PollEvent(&event);
@@ -42,6 +71,11 @@ void ballCollision() {
 	if (ball_y < background_min_h || ball_y > background_h - 30) {
 		ball_vel_y = -ball_vel_y;
 	}
+	int ballscaling = 20;
+	if (ball_y + ballscaling >= floor_y && ball_y + ballscaling <= floor_y + 30 &&
+		ball_x + ballscaling >= floor_x && ball_x + ballscaling <= floor_x + 60) {
+		ball_vel_y =- ball_vel_y;
+	}
 }
 
 int main(int argc, char** argv) {
@@ -54,9 +88,11 @@ int main(int argc, char** argv) {
 	SDL_Surface* ball = SDL_LoadBMP("ball.bmp");
 	SDL_Surface* background = SDL_LoadBMP("background.bmp");
 	SDL_Surface* floor = SDL_LoadBMP("floor.bmp");
+	brick = SDL_LoadBMP("brick.bmp");
 	SDL_Texture* ball_texture = SDL_CreateTextureFromSurface(renderer, ball);
 	SDL_Texture* background_texture = SDL_CreateTextureFromSurface(renderer, background);
 	SDL_Texture* floor_texture = SDL_CreateTextureFromSurface(renderer, floor);
+	brick_texture = SDL_CreateTextureFromSurface(renderer, brick);
 
 	while (!quit) {
 		EventHandler();
@@ -64,9 +100,31 @@ int main(int argc, char** argv) {
 		SDL_Rect floor_rect = { floor_x, floor_y, 60, 30 };
 		moveBall();
 		ballCollision();
+		InitializerBrick();
 		SDL_RenderCopy(renderer, background_texture, NULL, &background_rect);
 		SDL_RenderCopy(renderer, ball_texture, NULL, &ball_rect);
 		SDL_RenderCopy(renderer, floor_texture, NULL, &floor_rect);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][0]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][1]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][2]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][3]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][4]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][5]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[0][6]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][0]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][1]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][2]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][3]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][4]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][5]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[1][6]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][0]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][1]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][2]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][3]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][4]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][5]);
+		SDL_RenderCopy(renderer, brick_texture, NULL, &brick_rect[2][6]);
 		SDL_RenderPresent(renderer);
 		SDL_RenderClear(renderer);
 		SDL_Delay(3);
